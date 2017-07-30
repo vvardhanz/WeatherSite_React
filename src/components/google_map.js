cf0723df164a04c40f840ca967357591353bfceb
vvardhan <kvvardhanz@gmail.com>
2017-07-30 13:46:57 -0400

    Added google maps

---
+++ src/components/google_map.js
@@ -0,0 +1,22 @@
+import react, { Component } from 'react';
+
+class GoogleMap extends Component {
+
+  componentDidMount() {
+      new google.maps.Map(this.refs.map, {
+         zoom: 12,
+         center: {
+           lat: this.props.lat,
+           lng: this.props.lon
+         }
+      });
+  }
+
+
+      render() {
+          // this.ref.map
+          return <div ref="map" />;
+      }
+}
+
+export default GoogleMap;
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from  'react';
 import { connect } from 'react-redux';
 import Chart from '../components/charts';
+import GoogleMap from '../components/'
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,13 +9,17 @@ class WeatherList extends Component {
       const temps = cityData.list.map(weather => weather.main.temp);
       const pressures = cityData.list.map(weather => weather.main.pressure);
       const humidities = cityData.list.map(weather => weather.main.humidity);
+      const lon = cityData.city.coord.lon;
+      const lat = cityData.city.coord.lat;
+      const { lon, lat } = cityData.city.coord;
+
 
       return (
          <tr key={name}>
-            <td>{name}</td>
-            <td><Chart data={temps} color="orange" /></td>
-            <td><Chart data={pressures} color="green" /></td>
-            <td><Chart data={humidities} color="black" /></td>
+            <td><GoogleMap lon={lon} lat={lat} /></td>
+            <td><Chart data={temps} color="orange" units="K" /></td>
+            <td><Chart data={pressures} color="green" units="hPa" /></td>
+            <td><Chart data={humidities} color="black" units="%" /></td>
         </tr>
       );
   }
